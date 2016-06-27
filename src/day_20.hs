46269c2d6d4afb3ece3c776801fe0e2dc9fd7cb5
Jiayu <yi-jiayu@users.noreply.github.com>
2016-06-27 21:38:58 +0800

    Removed dependency on Data.List and Data.Maybe from day 20

--- src/day_20.hs
+++ src/day_20.hs
@@ -1,9 +1,7 @@
 import System.Environment
-import Data.List (findIndex)
-import Data.Maybe (fromJust)
 
 factors :: Int -> [Int]
-factors n = let f1 = filter ((== 0) . mod n) [2..(floor . sqrt $ fromIntegral n)]
+factors n = let f1 = filter ((== 0) . mod n) [2..(floor . sqrt . fromIntegral) n]
                 f2 = map (quot n) f1
             in 1:n:(f1 ++ f2)
 
@@ -13,11 +11,12 @@ presents = (* 10) . sum . factors
 presents' :: Int -> Int
 presents' h = (*11) . sum . filter (> quot h 50). factors $ h
 
-main = do (nPresents:_) <- getArgs
+main = do (nPresents':_) <- getArgs
+          let nPresents = read nPresents' :: Int
           putStrLn "Haskell Advent of Code"
           putStrLn "https://github.com/yi-jiayu/haskell-advent-of-code"
           putStrLn "Day 20\n"
-          putStrLn $ "Lowest house number to get " ++ nPresents ++ " presents:"
-          print $ fromJust $ findIndex (>= read nPresents) $ map presents [0..]
-          putStrLn $ "Lowest house number to get " ++ nPresents ++ " presents with lazy elves:"
-          print $ fromJust $ findIndex (>= read nPresents) $ map presents' [0..]
+          putStrLn $ "Lowest house number to get " ++ nPresents' ++ " presents:"
+          print $ (length . takeWhile (< nPresents) . map presents) [0..]
+          putStrLn $ "Lowest house number to get " ++ nPresents' ++ " presents with lazy elves:"
+          print $ (length . takeWhile (< nPresents) . map presents') [0..]
